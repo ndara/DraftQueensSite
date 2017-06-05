436c0de1ec64dd659467c67bc8687b735e9c22d9
ndara <nishanth.dara@gmail.com>
2017-06-05 16:59:12 -0700

    Added popup to add new lobby

--- public/Lobby/lobbyController.js
+++ public/Lobby/lobbyController.js
@@ -1,6 +1,25 @@
 app.controller('lobbyController',
- ['$scope', '$state', '$http',
- function($scope, $state, $http) {
+ ['$scope', '$state', '$http', '$uibModal', 'notifyDlg',
+ function($scope, $state, $http, $uibM, nDlg) {
 
    $scope.lobbies = [{name: "swag"}, {name: "yeet"}, {name: "swervefordaysbruh"}];
+
+   $scope.newLobby = function() {
+      $uibM.open({
+         templateUrl: 'Lobby/editLobbyDlg.template.html',
+         scope: $scope
+      }).result
+      .then(function(lobbyName) {
+         $scope.lobbies.push({name: lobbyName});
+      })
+      .then(function() {
+         $scope.lobbies = $scope.lobbies;
+      })
+      .catch(function(err) {
+         if(err) {
+            nDlg.show($scope, "An error occurred...\n" + err, "Error");
+         }
+      });
+   };
+
 }]);
