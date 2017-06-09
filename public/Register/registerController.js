4347c5e49bf519de7b3b5e93f61c9eb06959331c
ndara <nishanth.dara@gmail.com>
2017-06-09 15:16:58 -0700

    Added split screen view for draft screen

--- public/Register/registerController.js
+++ public/Register/registerController.js
@@ -1,6 +1,14 @@
 app.controller('registerController',
- ['$scope', '$state', '$http', 'notifyDlg', 'terms',
- function($scope, $state, $http, nDlg, terms) {
+ ['$scope', '$state', '$http', 'notifyDlg', 'terms', '$interval',
+ function($scope, $state, $http, nDlg, terms, $interval) {
+
+   $scope.num = 0;
+   
+   $scope.increaseNum = function() {
+      $scope.num++;
+   }
+
+   $interval($scope.increaseNum, 1000);
 
    $scope.showTerms = function() {
       nDlg.show($scope, terms.TERMS,
