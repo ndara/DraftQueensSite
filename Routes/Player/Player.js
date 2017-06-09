b59b74bbe767b6d5a1627a1da546b3c1c662c092
Gurjeevan Bains <gurjeevanb@gmail.com>
2017-06-08 23:24:34 -0700

    finished adding functionality for getting all players not in lobby, but added functionality not yet tested

--- Routes/Player/Player.js
+++ Routes/Player/Player.js
@@ -11,27 +11,56 @@ router.get('/', function(req, res) {
     'from Player where pos1 = ?';
    var noPrm = 'select id, fname, lname, pos1, height, weight, team ' +
     'from Player';
+   var noPrmLby = 'select P.id, fname, lname, pos1, height, weight, P.team ' +
+    'from Player P join Team T on (T.player1 <> P.id) and (T.player2 <> ' + 
+    'P.id) and (T.player3 <> P.id) and (T.player4 <> P.id) and (T.player5 ' +
+    '<> P.id) and (T.player6 <> P.id) and (T.player7 <> P.id) ' +
+    'where T.lobbyId = ?';
+   var queryLby = 'select P.id, fname, lname, pos1, height, weight, P.team ' +
+    'from Player P join Team T on (T.player1 <> P.id) and (T.player2 <> ' + 
+    'P.id) and (T.player3 <> P.id) and (T.player4 <> P.id) and (T.player5 ' +
+    '<> P.id) and (T.player6 <> P.id) and (T.player7 <> P.id) where ' +
+    'T.lobbyId = ? and pos1 = ?';
 
    if (req.query.position) {
-      req.cnn.chkQry(query, [req.query.position],
-      function(err, plys) {
+      if (req.query.lobby) {
+         req.cnn.chkQry(queryLby, [req.params.lobby, req.params.position],
+          function (err, plys){
+             if (!err) {
+               res.status(200).json(plys);
+            }
+            req.cnn.release();
+          });
+      }
 
-         if (!err) {
-            res.status(200).json(plys);
-         }
-         req.cnn.release();
-      });
+      else {
+         req.cnn.chkQry(query, [req.query.position], function(err, plys) {
+            if (!err) {
+               res.status(200).json(plys);
+            }
+            req.cnn.release();
+         });
+      }
    }
 
    else {
-      req.cnn.chkQry(noPrm, null,
-      function(err, plys) {
+      if (req.query.lobby) {
+         req.cnn.chkQry(noPrmLby, [req.params.lobby], function(err, plys) {
+            if (!err) {
+               res.status(200).json(plys);
+            }
+            req.cnn.release();
+         });
+      }
 
-         if (!err) {
-            res.status(200).json(plys);
-         }
-         req.cnn.release();
-      });
+      else {
+         req.cnn.chkQry(noPrm, null, function(err, plys) {
+            if (!err) {
+               res.status(200).json(plys);
+            }
+            req.cnn.release();
+         });
+      }
    }
 });
 
